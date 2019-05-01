c6b77aac714e387a1633e4353ea6526a01322b28
Daniel D <danieldouglas23@gmail.com>
2019-05-01 14:27:00 -0600

    first file structure commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route />
+        </Switch>
+      </>
+    </Router>
+
+
+  );
+}
+
+export default App;
