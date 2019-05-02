6f6ab8f5f9f6d5b0246a46e51dfe23d2a81b8c4c
Michael Diaz <poncediazdeleon@gmail.com>
2019-05-02 14:52:48 -0600

    nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Button, Nav, Form, FormControl, NavDropdown } from "react-bootstrap";
+import NavBar from "./components/NavBar/index";
+import Jumbo from "./components/Jumbotron";
+import Footer from "./components/Footer";
+import Search from "./pages/Search";
+
+
+
+
 
 function App() {
   return (
     <Router>
       <>
-      <h1>Hello</h1>
-        <Switch>
-          <Route />
-        </Switch>
+      <NavBar></NavBar>
+      <Jumbo></Jumbo>
+      <Footer></Footer>
+
+      <Switch >
+        <Route exact path="/Search" component={Search}/>
+
+      </Switch>
       </>
     </Router>
-    
+
 
   );
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,2 +1,25 @@
+import React, {Component} from "react";
 import Jumbotron from "../components/Jumbotron";
 import { Input, SubmitBtn, ClearBtn } from "../components/Form";
+
+class Search extends Component {
+
+    render() {
+        return (
+            <form>
+            <Input
+            city/>
+            <Input
+            Campany/>
+            <Input
+            Job/>
+            <Input
+            Catagory/>
+            <SubmitBtn></SubmitBtn>
+            <ClearBtn></ClearBtn>
+
+            </form>
+        )
+    }
+}
+export default Search;
