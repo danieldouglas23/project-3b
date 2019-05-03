4833bc6c56afdb93fca084fcc80ca48bacf58de9
badri m abdi <badriabdi877@gmail.com>
2019-05-03 16:49:53 -0600

    updated field

--- server.js
+++ server.js
@@ -4,10 +4,26 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const colors = require("colors");
+const session = require("express-session");
+const passport = require("passport");
+const logger = require("morgan");
+const flash = require('connect-flash');
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger("dev"));
+app.use(flash())
+app.use(express.static("public"));
+app.use(session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+}));
+
+// Define middleware here
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
