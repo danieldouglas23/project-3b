690e758e3cc27a86ce474e88720830a7c29c0f4d
badri m abdi <badriabdi877@gmail.com>
2019-05-03 09:47:05 -0600

    updated made

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,57 @@
+
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Auth from "./pages/Auth";
+import NoMatch from "./pages/NoMatch";
+import TopNav from "./components/TopNav";
+import Footer from "./components/Footer";
+import { Container } from 'reactstrap';
 
 function App() {
   return (
-    <Router>
-      <>
-      <h1>Hello</h1>
-        <Switch>
-          <Route />
-        </Switch>
-      </>
-    </Router>
-    
-
+      <Router>
+        <div>
+          <TopNav />
+          <Container>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/signup" render={(props) => <Auth {...props} action="signup" />} />
+              <Route exact path="/login" render={(props) => <Auth {...props} action="login" />} />
+              <Route exact path="/profile" component={Profile} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Container>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+
+
+//     <Router>
+//       <>
+//       <h1>Hello</h1>
+//         <Switch>
+//           <Route />
+//         </Switch>
+//       </>
+//     </Router>
+    
+
+//   );
+// }
+
+// export default App;
+
+
