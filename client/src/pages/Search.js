abcdce9edeacc1f7a64ec0ca238984a57a04cada
Michael Diaz <poncediazdeleon@gmail.com>
2019-05-06 13:44:46 -0600

    search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,25 +1,38 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import { Input, SubmitBtn, ClearBtn } from "../components/Form";
+import { Container } from "react-bootstrap";
+
 
 class Search extends Component {
 
     render() {
         return (
-            <form>
+           
+            <form> 
+
+            <div class="row">
+                   <div class="col-lg-3 col-centered"></div>
+                       
             <Input
-            city/>
+            placeholder="City"
+            />
             <Input
-            Campany/>
+            placeholder="Job Level"
+            />
             <Input
-            Job/>
-            <Input
-            Catagory/>
+            placeholder="Category"
+            />
+            </div> 
+            <div class="row">
+                    <div class="col-lg-5 col-centered"></div>
+                 
             <SubmitBtn></SubmitBtn>
             <ClearBtn></ClearBtn>
+            </div> 
 
             </form>
-        )
-    }
-}
+                    )
+                }
+            }
 export default Search;
