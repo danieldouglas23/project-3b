747a2e9968bd6b7e0239a4aff8671f4b7b8521c4
badri m abdi <badriabdi877@gmail.com>
2019-05-08 08:02:28 -0600

    removing authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from '../src/Routes';
+
 
 import registerServiceWorker from './registerServiceWorker';
-// import '@appbaseio/reactivesearch/dist/css/style.min.css';
+
 
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
