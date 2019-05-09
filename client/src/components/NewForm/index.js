0b71df4cf0bcd0a582c40fc7af3efc05c019ca57
Daniel D <danieldouglas23@gmail.com>
2019-05-09 10:39:00 -0600

    more css - responsive

--- client/src/components/NewForm/index.js
+++ client/src/components/NewForm/index.js
@@ -4,10 +4,10 @@ import "./style.css";
 const NewForm = (props) => {
     return (
         <form className="form-group" id="book-form">
-            <h1><i class="fas fa-laugh-squint"></i> Dad Joke Generator! <i class="fas fa-laugh-squint"></i></h1>
-            <button className="btn btn-success" type="submit" id="submit-button" value="Submit" onClick={props.onClick}>
-                Generate 10 New Jokes!!
-            </button>
+            <div class="form-actions">
+                <h1><i class="fas fa-laugh-squint"></i> Dad Joke Generator! <i class="fas fa-laugh-squint"></i></h1>
+                <button className="btn btn-success" type="submit" id="submit-button" value="Submit" onClick={props.onClick}>Generate 10 New Jokes!!</button>
+            </div>    
         </form>
     );
 }
